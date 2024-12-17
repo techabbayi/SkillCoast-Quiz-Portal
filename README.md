a74028e7a83ce9c039d4a3dd5e6b15d254a9cf94
Ravi Kumar Reddy <166979268+techabbayi@users.noreply.github.com>
2024-12-17 14:22:55 +0530

    Added Files

---
+++ README.md
@@ -0,0 +1,4 @@
+# SkillCoast quiz portal
+
+It is an Basic online portal in which one can attend the quiz online and view the results immeadiately after completing the quiz
+
---
+++ question.js
@@ -0,0 +1,48 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is HTML Full form ?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hi - Tech Machine Language",
+      "Hyper Text Mockup Language",
+      "Hyper Text Markup Language",
+      "Hype Text Machine Linguage"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is CSS Full Form ?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Castle Styling Sheets",
+      "Cascading Style Sheets",
+      "Control Sheet of HTML",
+      "Used to Style HTML Elements"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which CSS property is used to change the text color?",
+    answer: "color",
+    options: [
+      "color",
+      "background-color",
+      "font-size",
+      "text-align"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In HTML, what is the purpose of the <a> tag?",
+    answer: "To define a image",
+    options: [
+      "To define an image",
+      "To create a link",
+      "To define a section",
+      "To add a list item"
+    ]
+  },
+    
+];
+
